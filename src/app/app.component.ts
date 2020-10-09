b54715bbf8f2a38935456ad613102c9ef9f58aea
Vương Hữu Thiện <vuonghuuthientk@gmail.com>
2020-10-09 12:39:04 +0700

    Done Insert Text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, AfterViewInit, Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -19,12 +19,14 @@ export class AppComponent implements AfterViewChecked {
   }
   pages = [
     {
+      htmlContent: null,
       full: false
     },
   ]
   currentPage = 0;
+  currentChar = null;
 
-  heightA4WithoutPadding = this.sizePage.height - (this.paddingPage.top + this.paddingPage.bottom);
+  runAfterViewChecked = false;
   
   clickA4(i) {
     this.currentPage = i;
@@ -33,20 +35,43 @@ export class AppComponent implements AfterViewChecked {
   inputContent(char, i) {
     var element = document.getElementById('content-' + i)
     var heightContent = element.offsetHeight * 2.54 / 96; // Convert pixels to cm
-
-    if (heightContent > this.heightA4WithoutPadding) {
-      element.textContent = element.textContent.substring(0, element.textContent.length - 1);
+    this.pages[i].htmlContent = element.innerHTML;
+    console.log(this.pages);
+    if (Number(heightContent.toFixed(1)) > this.sizePage.height) { 
+      this.currentChar = char;
       this.pages[i].full = true;
       if (!this.pages[i + 1]) {
         this.pages.push({
+          htmlContent: null,
           full: false
         })
       }
       this.currentPage = i + 1;
+      this.runAfterViewChecked = true;
     }
   }
 
   ngAfterViewChecked() {
     document.getElementById('content-' + this.currentPage).focus();
+    if (this.runAfterViewChecked) {
+      if (this.currentChar) {
+        var str = this.pages[this.currentPage-1].htmlContent;
+        var index = str.lastIndexOf("</div>");
+        if (index != -1)
+          str = str.slice(0, index-1) + str.slice(index);
+        else 
+          str = str.slice(0, str.length - 1);
+        this.pages[this.currentPage-1].htmlContent = str;
+
+        this.pages[this.currentPage].htmlContent = this.currentChar + this.pages[this.currentPage].htmlContent;
+      }
+
+      var element = null;
+      for (let i = 0; i < this.pages.length; i++) {
+        element = document.getElementById('content-' + i);
+        element.innerHTML = this.pages[i].htmlContent;
+      }
+      this.runAfterViewChecked = false;
+    }
   }
 }
